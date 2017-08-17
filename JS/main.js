4b6298198e36e7484e4fc1b2c7bf462f9fddb809
Nicholas Thérien Roussel <NicolasNtr11@gmail.com>
2017-08-16 20:11:57 -0400

    Added name and animation
    Added name and animation

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,46 @@
 $(document).ready(function() {
+	
 	$("#imgDummy").on("click", function(e) {
+		
+		$(this).effect("highlight", "faster");
 		var newValue = parseInt($("#intExperience").text()) + 1;
 		$("#intExperience").text(newValue);
+	});
+	
+		$.confirm({
+		    title: 'Enter your name: ',
+    content: '' +
+    '<form action="" class="formName">' +
+    '<div class="form-group">' +
+    '<input type="text" placeholder="Your name" class="chosenName form-control" maxlength="15" required />' +
+    '</div>' +
+    '</form>',
+	boxWidth: '180px',
+    useBootstrap: false,
+	draggable:false,
+	backgroundDismissAnimation: 'glow',
+    buttons: {
+        formSubmit: {
+            text: 'Confirm',
+            btnClass: 'btn-blue',
+            action: function () {
+                var name = this.$content.find('.chosenName').val();
+                if(!name){
+                    return false;
+                }
+				
+				$("#lblName").text(name);
+            }
+        },
+    },
+    onContentReady: function () {
+        // bind to events
+        var jc = this;
+        this.$content.find('form').on('submit', function (e) {
+            // if the user submits the form by pressing enter in the field.
+            e.preventDefault();
+            jc.$$formSubmit.trigger('click'); // reference the button and click it
+        });
+    }
 	});
 });
